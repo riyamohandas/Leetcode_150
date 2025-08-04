fe25a3532b6447e56b7b43ae626537322f92ffe2
Riya S <162545620+riyamohandas@users.noreply.github.com>
2025-08-04 21:58:18 +0530

    Create Merge_sorted_Array.py

---
+++ Merge_sorted_Array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        i=m-1
+        j=n-1
+        k=n+m-1
+        while i>=0 and j>=0:
+            if nums2[j]>nums1[i]:
+                nums1[k]=nums2[j]
+                j-=1
+            else:
+                nums1[k]=nums1[i]
+                i-=1
+            k-=1
+        if j>=0:
+            nums1[k]=nums2[j]
+            j-=1
